da874f04d8f72eb81427fe837b889b873f5a61ad
materials1000 <50615637+materials1000@users.noreply.github.com>
2022-03-23 20:59:52 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS', 'localhost')
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify({
+        'host_name': HOST_NAME,
+    })
+    
+    # return "Hello World!"
 
 if __name__ == "__main__":
     application.run()
