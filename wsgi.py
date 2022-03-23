bee978ee45989da767ae272d174c0b4531ae3e57
materials1000 <50615637+materials1000@users.noreply.github.com>
2022-03-23 20:37:57 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,34 +1,31 @@
+import os
+import logging
+import socket
+from flask import Flask, jsonify
+
 from flask import Flask
 application = Flask(__name__)
 
-#tasks = [
-#    {
-#        'id': 1,
-#        'title': u'Buy groceries',
-#        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-#        'done': False
-#    },
-#    {
-#        'id': 2,
-#        'title': u'Learn Python',
-#        'description': u'Need to find a good Python tutorial on the web',
-#        'done': True
-#    },
-#    {
-#        'id': 3,
-#        'title': u'Explore IntelliJ',
-#        'description': u'This Docker Integration plugin is awesome',
-#        'done': False
-#    }
-#]
+HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS', 'localhost')
+APP_NAME = os.environ.get('OPENSHIFT_APP_NAME', 'flask')
+IP = os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1')
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+HOME_DIR = os.environ.get('OPENSHIFT_HOMEDIR', os.getcwd())
 
-#@app.route('/api/tasks', methods=['GET'])
-#def get_tasks():
-#    return jsonify({'tasks': tasks})
+log = logging.getLogger(__name__)
+app = Flask(__name__)
 
-@application.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!_test"
+    return jsonify({
+        'host_name': HOST_NAME,
+        'app_name': APP_NAME,
+        'ip': IP,
+        'port': PORT,
+        'home_dir': HOME_DIR,
+        'host': socket.gethostname()
+    })
+
 
 if __name__ == "__main__":
     application.run()
