f581a42c0f42e22f7e45c64f476d12ebdf3cde6e
materials1000 <50615637+materials1000@users.noreply.github.com>
2022-03-23 20:33:07 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,34 +1,34 @@
 from flask import Flask
 application = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': True
-    },
-    {
-        'id': 3,
-        'title': u'Explore IntelliJ',
-        'description': u'This Docker Integration plugin is awesome',
-        'done': False
-    }
-]
+#tasks = [
+#    {
+#        'id': 1,
+#        'title': u'Buy groceries',
+#        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+#        'done': False
+#    },
+#    {
+#        'id': 2,
+#        'title': u'Learn Python',
+#        'description': u'Need to find a good Python tutorial on the web',
+#        'done': True
+#    },
+#    {
+#        'id': 3,
+#        'title': u'Explore IntelliJ',
+#        'description': u'This Docker Integration plugin is awesome',
+#        'done': False
+#    }
+#]
 
-@app.route('/api/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+#@app.route('/api/tasks', methods=['GET'])
+#def get_tasks():
+#    return jsonify({'tasks': tasks})
 
-#@application.route("/")
-#def hello():
-#    return "Hello World!_test"
+@application.route("/")
+def hello():
+    return "Hello World!_test"
 
 if __name__ == "__main__":
     application.run()
