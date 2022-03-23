52744946d802c7b98d87b10d2922ce93deaefe42
materials1000 <50615637+materials1000@users.noreply.github.com>
2022-03-23 20:45:22 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,17 +3,19 @@ import logging
 import socket
 from flask import Flask, jsonify
 
-from flask import Flask
-application = Flask(__name__)
-
 HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS', 'localhost')
 APP_NAME = os.environ.get('OPENSHIFT_APP_NAME', 'flask')
 IP = os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1')
 PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
 HOME_DIR = os.environ.get('OPENSHIFT_HOMEDIR', os.getcwd())
 
+#from flask import Flask
+
+#application = Flask(__name__)
+
+
 log = logging.getLogger(__name__)
-app = Flask(__name__)
+application = Flask(__name__)
 
 @app.route('/')
 def hello():
